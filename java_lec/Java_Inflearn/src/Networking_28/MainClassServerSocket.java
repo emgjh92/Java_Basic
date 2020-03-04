0bfd04511c55d2d1dfd23c20944b99b789f3998d
Choi jaehyun <emgjh92@naver.com>
2020-03-04 10:33:03 +0900

    2020-03-04 (Java_Inflearn)
    2020-03-04 (Java_Inflearn)

---
+++ java_lec/Java_Inflearn/src/Networking_28/MainClassServerSocket.java
@@ -0,0 +1,38 @@
+package Networking_28;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MainClassServerSocket {
+
+	public static void main(String[] args) {
+		
+		ServerSocket serverSocket = null;
+		Socket socket = null;
+		
+		try {
+			serverSocket = new ServerSocket(9000);
+			System.out.println("클라이언트 맞을 준비 완료~~");
+			
+			socket = serverSocket.accept();
+			System.out.println("클라이언트 연결~~");
+			System.out.println("socket: " + socket);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(socket != null) socket.close();
+				if(serverSocket != null) serverSocket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			//웹 브라우저에서 localhost:9000 으로 접속 해 보자
+		}
+		
+		
+		
+	}
+	
+}
